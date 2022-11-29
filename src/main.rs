6ced5e71dbab9f20d41aff46bcfcd165244d1e62
Paul Welch <paulwelch@outlook.com>
2022-11-29 23:58:14 +0000

    hello world of graphics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+//TODO: write to bytestream redirected to '.ppm' file as image
+//use std::io;
+
 fn main() {
-    println!("Hello, world1!");
-    println!("Hello, world2!");
-    println!("Hello, world3!");
-    println!("Hello, world4!");
-    println!("Hello, world5!");
+
+    //Image
+    let image_width = 256;
+    let image_height = 256;
+
+    //Render
+    //std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    println!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in 0..(image_height-1) {
+        for i in 0..(image_width-1) {
+            let r = i / (image_width-1);
+            let g = (image_height-1-j) / (image_height-1);
+            let b = 0.25;
+
+            let ir = 255.999 * r as f32;
+            let ig = 255.999 * g as f32;
+            let ib = 255.999 * b as f32;
+
+            //std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            println!("{} {} {}\n", ir, ig, ib);
+        }
+    }
 }
