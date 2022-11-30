81f32ca24dd29bc9650e7d33731f0cc42d350922
Paul Welch <paulwelch@outlook.com>
2022-11-30 00:04:10 +0000

    fix type casts

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@ fn main() {
 
     for j in 0..(image_height-1) {
         for i in 0..(image_width-1) {
-            let r = i / (image_width-1);
-            let g = (image_height-1-j) / (image_height-1);
-            let b = 0.25;
+            let r: f32 = (i / (image_width-1)) as f32;
+            let g: f32 = ((image_height-1-j) / (image_height-1)) as f32;
+            let b: f32 = 0.25 as f32;
 
-            let ir = 255.999 * r as f32;
-            let ig = 255.999 * g as f32;
-            let ib = 255.999 * b as f32;
+            let ir = 255.999 as f32 * r;
+            let ig = 255.999 as f32 * g;
+            let ib = 255.999 as f32 * b;
 
             //std::cout << ir << ' ' << ig << ' ' << ib << '\n';
             println!("{} {} {}\n", ir, ig, ib);
